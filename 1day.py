c8550de55bbd78a8a41f3e2b7cd45ceec97cb545
rizarakhmat <84453893+rizarakhmat@users.noreply.github.com>
2022-12-03 15:17:31 +0100

    Day1/

---
+++ 1day.py
@@ -0,0 +1,22 @@
+file = open(r"C:\Users\rizar\Documents\PY PROGRAMMING\AdventCode2022\day1\input1.txt", "r")
+output = [0]
+
+def part1():
+    i = 0
+    for row in file:
+        if row == "\n":
+            i += 1
+            output.append(0)
+        else:
+            output[i] += int(row)
+    print(max(output))
+
+part1()
+
+
+def part2():
+    top = sorted(output, reverse = True)
+    top_3 = top[:3]
+    print(sum(top_3))
+
+part2()
